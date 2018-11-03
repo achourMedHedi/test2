45f08b804b3333e74ae3f49806cbcb4dca0bca12
achour <achourmedhedi@gmail.com>
2018-11-03 17:27:59 +0100

    Revert "First Commit"
    This reverts commit 864aac19d9bdf603177e1fab11c794fa1b79a29e.

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import Test from './Test'
-
-ReactDOM.render(<Test />, document.getElementById('root'));
-registerServiceWorker();
